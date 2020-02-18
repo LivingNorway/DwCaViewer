7cedc49c17a42120e0c4e42f88efef7437fd849e
Matthew Grainger <Matthew.grainger@nina.no>
2020-02-18 21:26:29 +0100

    Additional code to add to DwCaViewer

--- app.R
+++ app.R
@@ -12,6 +12,9 @@ library(tidyverse)
 library(xml2)
 library(leaflet)
 library(visdat)
+library(DT) # known error with Shiny Datatable fixed by calling DT explictly 
+#see https://stackoverflow.com/questions/58995381/shiny-datatable-error-datatables-warning-table-id-datatables-table-0-request
+
 
 # Define UI for application 
 ui <- fluidPage(
@@ -22,10 +25,13 @@ ui <- fluidPage(
     # Sidebar with for selection of data set (sleeping at the moment)
     sidebarLayout(
         sidebarPanel(selectInput("dataset", "Choose a dataset:",
-                choices = c("FeFo", "Statskog", "Fjellstyrene"),
+                choices = c("FeFo", "Statskog", "Fjellstyrene","Other"),
 
         ),
-        actionButton("run", "View data")),
+        actionButton("run", "View data"),
+        textInput("ID", label = h3("If you select 'Other' above please enter a gbif event ID"), 
+                  value = "e306fa70-381e-4330-8e68-1f447b46a850") #trying to see if any event dataset can be used 
+        ),
         
 
         # Show a plot of the generated distribution
@@ -49,12 +55,12 @@ ui <- fluidPage(
         
         tabPanel("View Event data", 
             h3("Event Data"),
-            dataTableOutput("table1")
+            DT::dataTableOutput("table1")
         ),
         
         tabPanel("View Occurence data", 
                  h3("Occurence Data"),
-                 dataTableOutput("table2")
+                 DT::dataTableOutput("table2")
         ),
         
         tabPanel("Explore Geographic patterns",
@@ -74,47 +80,109 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     observeEvent(input$run,{ #Run Button
+        if(input$dataset == "FeFo"){
+            datasetID <- "c47f13c1-7427-45a0-9f12-237aad351040"
+            dataset <- RJSONIO::fromJSON(paste0("http://api.gbif.org/v1/dataset/",datasetID,"/endpoint"))
+            endpoint_url <- dataset[[1]]$url 
+            
+            download.file(endpoint_url, destfile="data/temp.zip", mode="wb")
+            unzip ("data/temp.zip", exdir = "data")  
+            my_dat1 <- read.csv("data/event.txt", sep="\t", encoding = "UTF-8") %>% select(-id)
+            my_dat2 <- read.csv("data/occurrence.txt", sep="\t", encoding = "UTF-8") %>% select(-id) 
+            meta <- read_xml("data/eml.xml") %>% as_list() 
+            output$table1 <- DT::renderDataTable(my_dat1)
+            output$table2 <- DT::renderDataTable(my_dat2)
+            output$DOI1 <- renderText(attr(meta$eml$additionalMetadata$metadata$gbif$citation,"identifier"))
+            output$gbif_citation <- renderText(meta$eml$additionalMetadata$metadata$gbif$citation[[1]])        
+            output$list_files <- renderTable(as.matrix(dir("data", pattern="\\.txt$"), ncol=1))
         
-        datasetID <- "c47f13c1-7427-45a0-9f12-237aad351040"
-        dataset <- RJSONIO::fromJSON(paste0("http://api.gbif.org/v1/dataset/",datasetID,"/endpoint"))
-        endpoint_url <- dataset[[1]]$url 
-        
-        download.file(endpoint_url, destfile="data/temp.zip", mode="wb")
-        unzip ("data/temp.zip", exdir = "data")
-        
-        my_dat1 <- read.csv("data/event.txt", sep="\t", encoding = "UTF-8") %>% select(-id)
-        my_dat2 <- read.csv("data/occurrence.txt", sep="\t", encoding = "UTF-8") %>% select(-id) 
-        
-        meta <- read_xml("data/eml.xml") %>% as_list() 
-        
-        
-        output$table1 <- renderDataTable(my_dat1)
-        output$table2 <- renderDataTable(my_dat2)
-        
-        
-        output$DOI1 <- renderText(attr(meta$eml$additionalMetadata$metadata$gbif$citation,"identifier"))
-        output$gbif_citation <- renderText(meta$eml$additionalMetadata$metadata$gbif$citation[[1]])        
-        output$list_files <- renderTable(as.matrix(dir("data", pattern="\\.txt$"), ncol=1))
-        
-    })
+        }
+        if(input$dataset=="Statskog"){
+            datasetID <- "6a948a1c-7e23-4d99-b1c1-ec578d0d3159"
+            dataset <- RJSONIO::fromJSON(paste0("http://api.gbif.org/v1/dataset/",datasetID,"/endpoint"))
+            endpoint_url <- dataset[[1]]$url 
+            
+            
+            download.file(endpoint_url, destfile="data/temp.zip", mode="wb")
+            unzip ("data/temp.zip", exdir = "data") 
+            my_dat1 <- read.csv("data/event.txt", sep="\t", encoding = "UTF-8") %>% select(-id)
+            my_dat2 <- read.csv("data/occurrence.txt", sep="\t", encoding = "UTF-8") %>% select(-id) 
+            meta <- read_xml("data/eml.xml") %>% as_list() 
+            output$table1 <- DT::renderDataTable(my_dat1)
+            output$table2 <- DT::renderDataTable(my_dat2)
+            output$DOI1 <- renderText(attr(meta$eml$additionalMetadata$metadata$gbif$citation,"identifier"))
+            output$gbif_citation <- renderText(meta$eml$additionalMetadata$metadata$gbif$citation[[1]])        
+            output$list_files <- renderTable(as.matrix(dir("data", pattern="\\.txt$"), ncol=1))
+           
+        }
+        if(input$dataset=="Fjellstyrene"){
+            datasetID <- "b49a2978-0e30-4748-a99f-9301d17ae119"
+            dataset <- RJSONIO::fromJSON(paste0("http://api.gbif.org/v1/dataset/",datasetID,"/endpoint"))
+            endpoint_url <- dataset[[1]]$url 
+            
+            download.file(endpoint_url, destfile="data/temp.zip", mode="wb")
+            unzip ("data/temp.zip", exdir = "data")
+            my_dat1 <- read.csv("data/event.txt", sep="\t", encoding = "UTF-8") %>% select(-id)
+            my_dat2 <- read.csv("data/occurrence.txt", sep="\t", encoding = "UTF-8") %>% select(-id) 
+            meta <- read_xml("data/eml.xml") %>% as_list() 
+            output$table1 <- DT::renderDataTable(my_dat1)
+            output$table2 <- DT::renderDataTable(my_dat2)
+            output$DOI1 <- renderText(attr(meta$eml$additionalMetadata$metadata$gbif$citation,"identifier"))
+            output$gbif_citation <- renderText(meta$eml$additionalMetadata$metadata$gbif$citation[[1]])        
+            output$list_files <- renderTable(as.matrix(dir("data", pattern="\\.txt$"), ncol=1))
+            
+        }
+        if(input$dataset=="Other"){
+            dataset <- RJSONIO::fromJSON(paste0("http://api.gbif.org/v1/dataset/",input$ID,"/endpoint"))
+            endpoint_url <- dataset[[1]]$url 
+            
+            download.file(endpoint_url, destfile="data/temp.zip", mode="wb")
+            unzip ("data/temp.zip", exdir = "data")
+            my_dat1 <- read.csv("data/event.txt", sep="\t", encoding = "UTF-8") %>% select(-id)
+            my_dat2 <- read.csv("data/occurrence.txt", sep="\t", encoding = "UTF-8") %>% select(-id) 
+            meta <- read_xml("data/eml.xml") %>% as_list() 
+            output$table1 <- DT::renderDataTable(my_dat1)
+            output$table2 <- DT::renderDataTable(my_dat2)
+            output$DOI1 <- renderText(attr(meta$eml$additionalMetadata$metadata$gbif$citation,"identifier"))
+            output$gbif_citation <- renderText(meta$eml$additionalMetadata$metadata$gbif$citation[[1]])        
+            output$list_files <- renderTable(as.matrix(dir("data", pattern="\\.txt$"), ncol=1))
+            
+            
+        }
+                })
+    
+    plotdata<-eventReactive(input$run,{
+        plotdata<-read.csv("data/event.txt", sep="\t", encoding = "UTF-8")
+        plotdata%>%select(-id)->plotdata})
+    
+    
+   #                         })})
+   #  
     
     ##
-    output$mymap <- renderLeaflet({
+
+    
+    output$mymap <- renderLeaflet({ #this is very slow - need to improve the plotdata part
+        plotdata() %>%
+            dplyr::select(decimalLatitude, decimalLongitude) %>%
+            drop_na() -> plot.dat
         leaflet() %>%
             addProviderTiles(providers$Esri.NatGeoWorldMap,
                              options = providerTileOptions(noWrap = TRUE)
-            )
+            ) %>%
+            addMarkers(data=plot.dat,lng = ~decimalLongitude, lat=~decimalLatitude)
     })
-    
+
     ##
     output$dataviz1 <- renderPlot({
-        dat <- my_dat1()
+        dat <- data.frame(plotdata())
         vis_guess(dat)
-        
+
     })
-    
+
     
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
